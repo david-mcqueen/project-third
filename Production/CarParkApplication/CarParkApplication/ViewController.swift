6df9bfe7bfdf2aefa1ba307cac4be6486c3a5acb
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-13 14:29:47 +0100

    Added in iBeacon functionality in a very primitive form. Needs improving extensively for the application

--- Production/CarParkApplication/CarParkApplication/ViewController.swift
+++ Production/CarParkApplication/CarParkApplication/ViewController.swift
@@ -7,18 +7,62 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager();
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "EBEFD083-70A2-47C8-9837-E7B5634DF524"), identifier: "Estimotes")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self;
+        
+        
+        //Request permission to access beacons - Whilst the app is in Foreground
+        if(CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse){
+            locationManager.requestWhenInUseAuthorization();
+        }
+        
+        //TODO:- Handle the user denying the location request
+        
+        
+        //Start looking for beacons
+        locationManager.startRangingBeaconsInRegion(region);
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        //This is called when:
+        // A new beacon is within range
+        // A beacon goe out of range
+        // A beacon gets closer / further away
+        
+        //the CLBeacon object (beacons) is an array of all beacons in region
+        
+        // Look on http://willd.me/posts/getting-started-with-ibeacon-a-swift-tutorial for more properties that can be accessed
+        // Proximity / rssi / accuracy
+        
+        //Disregard beacons that are Unknown proximity
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown };
+        
+        //TODO:- Organise the array in closest first
+        // It looks like it might already do that, however cant be trusted
+        
+        
+        
+        println(knownBeacons);
+    }
+    
+  
 
 
 }
