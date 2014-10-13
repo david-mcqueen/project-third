d574d61ba5e7654da9602e74db96f1176e0db6bb
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-13 17:26:33 +0100

    Minor modifications to the iBeacon functionality

--- Production/CarParkApplication/CarParkApplication/ViewController.swift
+++ Production/CarParkApplication/CarParkApplication/ViewController.swift
@@ -12,14 +12,13 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager();
-    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "EBEFD083-70A2-47C8-9837-E7B5634DF524"), identifier: "Estimotes")
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "EBEFD083-70A2-47C8-9837-E7B5634DF524"), identifier: "CarPark")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locationManager.delegate = self;
-        
         
         //Request permission to access beacons - Whilst the app is in Foreground
         if(CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse){
@@ -29,8 +28,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         //TODO:- Handle the user denying the location request
         
         
-        //Start looking for beacons
-        locationManager.startRangingBeaconsInRegion(region);
+        //Start looking for beacons so long as we have permission
+        if(CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse){
+                
+                locationManager.startRangingBeaconsInRegion(region);
+        }
         
     }
     
@@ -56,14 +58,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         //TODO:- Organise the array in closest first
         // It looks like it might already do that, however cant be trusted
+        if(knownBeacons.count > 0){
+            println(knownBeacons);
+        }
         
-        
-        
-        println(knownBeacons);
     }
     
-  
-
 
 }
 
