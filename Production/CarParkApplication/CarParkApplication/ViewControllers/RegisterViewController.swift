f0e38daef68fee30c552e43b6ff8255b4f829a02
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-13 19:41:45 +0100

    Made some advances on the registration screen

--- Production/CarParkApplication/CarParkApplication/ViewControllers/RegisterViewController.swift
+++ Production/CarParkApplication/CarParkApplication/ViewControllers/RegisterViewController.swift
@@ -13,14 +13,14 @@ class RegisterViewController: UIViewController {
    
     //MARK:- Variables & Constants
     
-    @IBOutlet weak var Scroller: UIScrollView!
+    @IBOutlet weak var Scroller: UIScrollView! //Un-needed?
     @IBOutlet weak var FirstNameInput: UITextField!
     @IBOutlet weak var SurNameInput: UITextField!
     @IBOutlet weak var EmailInput: UITextField!
     @IBOutlet weak var EmailInputConfirm: UITextField!
     @IBOutlet weak var PasswordInput: UITextField!
     @IBOutlet weak var PasswordInputConfirm: UITextField!
-    @IBOutlet weak var VehicleDetailsButton: UIBarButtonItem!
+    @IBOutlet weak var RegisterButton: UIBarButtonItem!
     
     //MARK:- Required Functions
     
@@ -29,18 +29,15 @@ class RegisterViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        //TODO:- Disable VehicleDetailsButton until all fields are complete / have content
-        
-        
-        
     }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         let sceenSize: CGRect = UIScreen.mainScreen().bounds;
-        Scroller.scrollEnabled = true;
-        Scroller.contentSize = CGSize(width:sceenSize.width, height: 1000);
+       // Scroller.scrollEnabled = true;
+       // Scroller.contentSize = CGSize(width:sceenSize.width, height: 1000);
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,16 +47,16 @@ class RegisterViewController: UIViewController {
     
     //MARK:- Actions
     
-    //Vehicle Details button has been pressed
-    @IBAction func VehicleDetails(sender: AnyObject) {
+    @IBAction func RegisterPressed(sender: AnyObject) {
+        
         //TODO:- Process new registration
         /*
-            1) Validate details are good
-                If bad then stay on page
-            2) If details are good, pass them to the server
-            3) Get a response back from the server
-                If success: Proceed to vehicle details
-                If Failure: take appropiate action dependent on failed reason.
+        1) Validate details are good
+        If bad then stay on page
+        2) If details are good, pass them to the server
+        3) Get a response back from the server
+        If success: Proceed to vehicle details
+        If Failure: take appropiate action dependent on failed reason.
         */
         
         var result: String;
@@ -67,11 +64,22 @@ class RegisterViewController: UIViewController {
         
         if(validateUserDetails(userInput)){
             sendUserDetails(userInput);
+            NSLog(userInput.FirstName);
+            NSLog(userInput.SurName);
+            NSLog(userInput.Email);
+            NSLog(userInput.ConfirmEmail!);
+            NSLog(userInput.Password);
+            NSLog(userInput.ConfirmPassword!);
+            if (validateEmail(userInput.Email)){
+                NSLog("emailed Passed");
+            }else {
+                NSLog("email Failed");
+            }
+            
         }else{
             result = "failed";
         }
     }
-    
     
     //MARK:- Functions
     
@@ -87,6 +95,13 @@ class RegisterViewController: UIViewController {
         */
         
         return true;
+    }
+    
+    func validateEmail(inputEmail: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+        
+        var emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx);
+        return emailTest.evaluateWithObject(inputEmail);
     }
     
     func sendUserDetails(validatedInput:UserRegistration){
