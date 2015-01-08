945d1e1cfcf28e97c3a8a9ad56887e08f3a73dbf
DavidMcQueen <dave.j.mcqueen@gmail.com>
2015-01-08 15:05:39 +0000

    Connect the new createVehicleViewController into the storyboards, replacing the old view (which used SwiftForms)

--- CarParkApplication/ViewControllers/CreateVehicleViewController.swift
+++ CarParkApplication/ViewControllers/CreateVehicleViewController.swift
@@ -20,11 +20,14 @@ class CreateVehicleViewController: UITableViewController,UIPickerViewDataSource,
     @IBOutlet var registrationLabel: UITextField!
     var makePicker: UIPickerView = UIPickerView();
     var modelPicker: UIPickerView = UIPickerView();
-    var makeData = ["Mozzarella","Gorgonzola","Provolone","Brie","Maytag Blue","Sharp Cheddar","Monterrey Jack","Stilton","Gouda","Goat Cheese", "Asiago"]
-    var modelData = ["Mozzarella","Gorgonzola","Provolone","Brie","Maytag Blue","Sharp Cheddar","Monterrey Jack","Stilton","Gouda","Goat Cheese", "Asiago"]
+    var makePickerData = ["Loading..."];
+    var makeDictionary: [String: String] = ["": ""];
+    var modelPickerData = ["Loading..."];
+    var modelDictionary: [String: String] = ["": ""];
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Submit", style: .Bordered, target: self, action: "submit:")
         makePicker.dataSource = self;
         makePicker.delegate = self;
         makePicker.tag = 0;
@@ -40,6 +43,9 @@ class CreateVehicleViewController: UITableViewController,UIPickerViewDataSource,
         modelInput.inputView = modelPicker;
         modelInput.frame = CGRectZero;
         modelInput.hidden = true;
+        modelInput.userInteractionEnabled = false;
+        
+        populateMakes();
         
     }
     
@@ -55,6 +61,10 @@ class CreateVehicleViewController: UITableViewController,UIPickerViewDataSource,
         }
     }
     
+    func submit(_: UIBarButtonItem!) {
+        println("Register new vehicle with user")
+    }
+    
     //MARK: - Delegates and data sources
     //MARK: Data Sources
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -62,29 +72,83 @@ class CreateVehicleViewController: UITableViewController,UIPickerViewDataSource,
     }
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if (pickerView.tag == 0){
-            return makeData.count
+            return makePickerData.count
         }else{
-            return modelData.count
+            return modelPickerData.count
         }
     }
     
     //MARK: Delegates
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         if (pickerView.tag == 0){
-            return makeData[row]
+            return makePickerData[row]
         }else{
-            return modelData[row]
+            return modelPickerData[row]
         }
         
     }
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var result: (String, String) = ("","");
         if (pickerView.tag == 0){
-            makeLabel.text = makeData[row]
+            //Display the selected car make
+            makeLabel.text = makePickerData[row]
+            
+            //Make the model read only, until the models for that make are loaded
+            modelInput.userInteractionEnabled = false;
+            modelLabel.text = "Loading models..."
+            
+            populateModels(self.makeDictionary[makePickerData[row]]!);
+            
         }else{
-            modelLabel.text = modelData[row]
+            modelLabel.text = modelPickerData[row]
         }
         
     }
     
+    func populateMakes(){
+        //Get the makes from the API
+        getMakes({(success: Bool, vehicleMakes: [CarMake]) -> () in
+            if(success) {
+                
+                var listOfMakes: [String] = []
+                
+                for make in vehicleMakes{
+                    listOfMakes.append(make.make_display.description);
+                    
+                    //Save the make name & ID in a dictionary
+                    self.makeDictionary[make.make_display.description] = make.make_id.description;
+                }
+                //TODO:- Handle no data being returned from the server (empty list for picker etc)
+                self.makePickerData = listOfMakes;
+                self.makeLabel.text = "Select Make"
+                
+            }else{
+                println("Something went wrong");
+            }
+        });
+    }
+    
+    func populateModels(selectedMakeID: String){
+        //Get the makes from the API
+        getModels(selectedMakeID, {(success: Bool, vehicleModels: [CarModel]) -> () in
+            if(success) {
+                var listOfModels: [String] = []
+                
+                for model in vehicleModels{
+                    listOfModels.append(model.model_name.description);
+                    self.modelDictionary[model.model_name.description] = model.model_make_id.description;
+                }
+                //TODO:- Handle no data being returned from the server (empty list for picker etc)
+                self.modelPickerData = listOfModels;
+                self.modelLabel.text = "Select Model";
+                self.modelInput.userInteractionEnabled = true;
+                
+            }else{
+                println("Something went wrong");
+            }
+        });
+    }
+
 }
+
