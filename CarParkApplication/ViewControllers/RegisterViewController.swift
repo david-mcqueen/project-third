6b272b0c47258d4c7d301042a6b5e5aab374e60d
DavidMcQueen <dave.j.mcqueen@gmail.com>
2015-01-25 12:43:10 +0000

    Finished work on changing the UserRegister to use the tabView instead of SwiftForms

--- CarParkApplication/ViewControllers/RegisterViewController.swift
+++ CarParkApplication/ViewControllers/RegisterViewController.swift
@@ -39,6 +39,100 @@ class RegisterViewController: UITableViewController {
     }
     
     @IBAction func registerUserPressed(sender: AnyObject) {
+        let (inputsCompleted, missingInputs) = allFieldsCompleted();
+        
+        if (!inputsCompleted){
+            var validationMessage = "Please complete all fields!."
+            for (inputField) in missingInputs{
+                validationMessage = validationMessage + "\n" +  inputField;
+            }
+            var validationAlert = UIAlertView(title: "Missing data!", message: validationMessage, delegate: nil, cancelButtonTitle: "Okay.")
+            validationAlert.show();
+        }else{
+            var newUser = UserRegistration(
+                    _firstName: txtFirstName.text,
+                    _surname: txtLastName.text,
+                    _email: txtEmail.text,
+                    _password: txtPassword.text,
+                    _phoneNumber: txtPhone.text);
+            
+            if(!newUser.validEmailPattern() || !newUser.validPasswordPattern() || !newUser.validPhonePattern()){
+                var invalidInputsMessage = "Please correct your inputs"
+                if (!newUser.validEmailPattern()){
+                    invalidInputsMessage = invalidInputsMessage + "\n" + "Invalid Email"
+                }
+                if (!newUser.validPhonePattern()){
+                    invalidInputsMessage = invalidInputsMessage + "\n" + "Invalid Phone Number"
+                }
+                if (!newUser.validPasswordPattern()){
+                    invalidInputsMessage = invalidInputsMessage + "\n" + "Invalid Password"
+                }
+                
+                
+                var invalidInputsAlert = UIAlertView(title: "Incomplete data!", message: invalidInputsMessage, delegate: nil, cancelButtonTitle: "Okay.")
+                invalidInputsAlert.show();
+
+                return;
+            }
+            
+            
+            registerUser(newUser, {(success: Bool, token: String, error: String?) -> () in
+                var alert = UIAlertView(title: "Success!", message: token, delegate: nil, cancelButtonTitle: "Okay.")
+                if(success) {
+                    alert.title = "Success!"
+                    println(token)
+                }
+                else {
+                    alert.title = "Failed : ("
+                    alert.message = token
+                }
+
+                // Move to the UI thread
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // Show the alert
+                    alert.show()
+                    if(success){
+                        User.sharedInstance.token = token;
+                        User.sharedInstance.FirstName = newUser.FirstName;
+                        User.sharedInstance.Surname = newUser.SurName;
+                        let createVehicleViewController = self.storyboard?.instantiateViewControllerWithIdentifier("CreateVehicleViewController") as CreateVehicleViewController;
+                        self.navigationController?.pushViewController(createVehicleViewController, animated: true);
+                    }
+                });
+                
+                }
+          );
+            
+        }
+        
+    }
+    
+    func allFieldsCompleted() -> (Bool, [String]){
+        var success = true;
+        var missingInputs: [String] = [];
+        
+        if(txtFirstName.text == nil || txtFirstName.text == ""){
+            success = false;
+            missingInputs.append("First Name");
+        }
+        if(txtLastName.text == nil || txtLastName.text == ""){
+            success = false;
+            missingInputs.append("Last Name");
+        }
+        if(txtEmail.text == nil || txtEmail.text == ""){
+            success = false;
+            missingInputs.append("Email");
+        }
+        if(txtPhone.text == nil || txtPhone.text == ""){
+            success = false;
+            missingInputs.append("Phone Number");
+        }
+        if(txtPassword.text == nil || txtPassword.text == ""){
+            success = false;
+            missingInputs.append("Phone Number");
+        }
+        
+        return (success, missingInputs);
     }
     /// MARK: Actions
     
