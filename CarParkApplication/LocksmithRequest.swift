654a9e51fd606266a558948c55888bd1d8587c5f
DavidMcQueen <dave.j.mcqueen@gmail.com>
2015-02-12 20:18:22 +0000

    Added LockSmith credit at the top of the files, including the Github URL

--- CarParkApplication/LocksmithRequest.swift
+++ CarParkApplication/LocksmithRequest.swift
@@ -5,6 +5,12 @@
 //  Copyright (c) 2014 Colour Coding. All rights reserved.
 //
 
+
+/*
+    Used with permission under the MIT license
+    https://github.com/matthewpalmer/Locksmith
+*/
+
 import UIKit
 import Security
 
